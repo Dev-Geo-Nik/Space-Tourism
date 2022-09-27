ac63d87f1bc2222d7207db8a42d981a303c679bf
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2022-09-27 12:07:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from "./pages/home/Home";
+
+
+const App: React.FC = () => {
+  return (
+    <>
+    <Home/>
+    </>
+  );
+};
+
+export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import styles from './home.module.scss';
+
+const  Home :React.FC = () => {
+
+  return (
+<div className={`container  flow`}>
+    <h1>Design system</h1>
+    <section id="colors">x</section>
+</div>
+);
+};
+
+export default Home;
+
