072e319d27e6ba7996dd3961eef65e5c2d18b9a7
George Sparrow <dev.georgios.nikolaidis@gmail.com>
2022-09-28 15:02:50 +0200

    add design scss classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import DesignSystem from "./pages/DesignSystem";
 import Home from "./pages/home/Home";
 
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <>
-    <Home/>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/design" element={<DesignSystem />} />
+        {/* <Route path="*" element={<Error>}/> */}
+      </Routes>
+    </Router>
   );
 };
 
